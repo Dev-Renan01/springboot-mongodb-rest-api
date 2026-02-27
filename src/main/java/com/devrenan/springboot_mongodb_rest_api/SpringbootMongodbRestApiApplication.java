4c9e29f4e4004343d2fc4ec550dcb292c092d2a6
Thiago Renan <thiagorenan1108@gmail.com>
2026-02-26 23:48:25 -0300

    Projeto criado

---
+++ src/main/java/com/devrenan/springboot_mongodb_rest_api/SpringbootMongodbRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.devrenan.springboot_mongodb_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrenan/springboot_mongodb_rest_api/SpringbootMongodbRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrenan.springboot_mongodb_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
